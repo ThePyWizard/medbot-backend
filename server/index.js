548255e199deb5cda5e793de1a337eecf07346e2
Jishnu M M <jishnumm4312@gmail.com>
2024-03-25 09:30:46 +0530

    vercel.json

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ const app = express()
 
 const PORT = process.env.PORT || 5000
 
-const allowedOrigins = ['http://localhost:5173'];
+const allowedOrigins = ['http://localhost:5173', 'https://medbot-frontend.vercel.app'];
 
 const corsOptions = {
     origin: function (origin, callback) {
